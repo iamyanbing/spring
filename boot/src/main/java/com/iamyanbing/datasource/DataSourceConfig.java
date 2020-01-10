fe7b46da97beeeb4d8999d771fbe26f6a524734b
iamyanbing <757750514@qq.com>
2020-01-10 16:21:20 +0800

    Create DataSourceConfig.java

---
+++ boot/src/main/java/com/iamyanbing/datasource/DataSourceConfig.java
@@ -0,0 +1,61 @@
+package com.createivearts.adminmanager.config;
+
+import com.createivearts.adminmanager.datasource.MyRoutingDataSource;
+import com.createivearts.adminmanager.enums.DBTypeEnum;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author huangyanbing
+ * @date 2019-10-14 15:14
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.slave")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.dwduser")
+    public DataSource dwduserDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    /**
+     * 配置路由数据源，前2个数据源都是为了生成第这个路由数据源，而且后续我们只用这个路由数据源。
+     *
+     * @param masterDataSource
+     * @param slaveDataSource
+     * @return
+     */
+    @Bean
+    public DataSource myRoutingDataSource(@Qualifier("masterDataSource") DataSource masterDataSource,
+                                          @Qualifier("dwduserDataSource") DataSource dwduserDataSource,
+                                          @Qualifier("slaveDataSource") DataSource slaveDataSource) {
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DBTypeEnum.MASTER, masterDataSource);
+        targetDataSources.put(DBTypeEnum.SLAVE, slaveDataSource);
+        targetDataSources.put(DBTypeEnum.DWDUSER, dwduserDataSource);
+        MyRoutingDataSource myRoutingDataSource = new MyRoutingDataSource();
+        myRoutingDataSource.setDefaultTargetDataSource(masterDataSource);
+        myRoutingDataSource.setTargetDataSources(targetDataSources);
+        return myRoutingDataSource;
+    }
+
+}
